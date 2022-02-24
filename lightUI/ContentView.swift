8997a2c0f00a2da87f8d2b38ba57db36855dce9a
Adham <adam.bolotov@gmail.com>
2022-02-24 04:52:34 +0100

    start

--- lightUI/ContentView.swift
+++ lightUI/ContentView.swift
@@ -1,16 +1,41 @@
-//
-//  ContentView.swift
-//  lightUI
-//
-//  Created by user213083 on 2/24/22.
-//
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 35.0) {
+            Spacer()
+            
+          Circle()
+                .frame(width: 150, height: 150)
+                .foregroundColor(.red)
+          Circle()
+                  .frame(width: 150, height: 150)
+                  .foregroundColor(.yellow)
+          Circle()
+                  .frame(width: 150, height: 150)
+                  .foregroundColor(.green)
+            
+            Spacer()
+            
+            Button {
+                //
+            } label: {
+               
+                    RoundedRectangle(cornerRadius: 35)
+                        .frame(width: 170, height: 115)
+                        .foregroundColor(.black)
+                        .overlay(RoundedRectangle(cornerRadius: 25)
+                                    .frame(width: 120, height: 80)
+                                    .foregroundColor(.gray))
+                    
+                                        
+               
+            }
+           
+            Spacer()
+        }
     }
 }
 
@@ -19,3 +44,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
