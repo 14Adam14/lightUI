638b07a12425b4ae18d0cf169fdad74182d345e4
Adham <adam.bolotov@gmail.com>
2022-02-24 04:48:16 +0100

    Initial Commit

---
+++ lightUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lightUI
+//
+//  Created by user213083 on 2/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lightUI/lightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  lightUIApp.swift
+//  lightUI
+//
+//  Created by user213083 on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct lightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
