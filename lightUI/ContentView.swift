30ea6519ead57a0ce7e745cb03c8da452dbf6a70
Adham <adam.bolotov@gmail.com>
2022-02-25 05:28:47 +0100

    process

--- lightUI/ContentView.swift
+++ lightUI/ContentView.swift
@@ -4,36 +4,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 90) {
-
+        
+        ZStack {
+           Rectangle()
+                .background(Color.black)
+                .ignoresSafeArea()
             
-            VStack(spacing: 30) {
-                Circle()
-                    .frame(width: 150, height: 150)
-                    .foregroundColor(.red)
-                Circle()
+            VStack(spacing: 90) {
+                
+                VStack(spacing: 30) {
+                    
+                    Circle()
                         .frame(width: 150, height: 150)
-                        .foregroundColor(.yellow)
-                Circle()
-                        .frame(width: 150, height: 150)
-                        .foregroundColor(.green)
-            }
-         
-            
-            
-            Button {
-                //
-            } label: {
-               
-                    RoundedRectangle(cornerRadius: 35)
-                        .frame(width: 170, height: 115)
-                        .foregroundColor(.black)
-                        .overlay(RoundedRectangle(cornerRadius: 25)
-                                    .frame(width: 120, height: 80)
-                                    .foregroundColor(.gray))
-            }
+                        .foregroundColor(.red)
+                        .blur(radius: 2)
+                        .shadow(color: .red, radius: 11)
+                    
+                    Circle()
+                            .frame(width: 150, height: 150)
+                            .foregroundColor(.yellow)
+                            .blur(radius: 2)
+                            .shadow(color: .yellow, radius: 0)
 
+                            .opacity(0.1)
+                    
+                    Circle()
+                            .frame(width: 150, height: 150)
+                            .foregroundColor(.green)
+                            .blur(radius: 2)
+                            .shadow(color: .green, radius: 0)    // radius 11
+                    
+                            .opacity(0.1)
+                }
+             
+                
+                
+                Button {
+                    //
+                } label: {
+                   
+                        RoundedRectangle(cornerRadius: 35)
+                            .frame(width: 170, height: 115)
+                            .foregroundColor(.orange)
+                            .overlay(RoundedRectangle(cornerRadius: 25)
+                                        .frame(width: 120, height: 80)
+                                        .foregroundColor(.gray))
+                }
+
+            }
         }
+        
+       
     }
 }
 
