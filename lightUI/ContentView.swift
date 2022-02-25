b64c997a35bd86d4ae17b0f4394456a8a078cd96
Adham <adam.bolotov@gmail.com>
2022-02-25 07:33:02 +0100

    process yea

--- lightUI/ContentView.swift
+++ lightUI/ContentView.swift
@@ -8,7 +8,7 @@ import SwiftUI
 struct ContentView: View {
     
     @State var redLightShadow: Double = 0
-    @State var yellowwLightShadow: Double = 0
+    @State var yellowLightShadow: Double = 0
     @State var greenLightShadow: Double = 0
     
     @State  var redLightOpacity: Double = 0.1
@@ -42,15 +42,15 @@ struct ContentView: View {
                         .frame(width: 150, height: 150)
                         .foregroundColor(.yellow)
                         .blur(radius: 2)
-                        .shadow(color: .yellow, radius: yellowwLightShadow)
+                        .shadow(color: .yellow, radius: yellowLightShadow)
                     
-                        .opacity(greenLightOpacity)
+                        .opacity(yellowLightOpacity)
                     
                     Circle()
                         .frame(width: 150, height: 150)
                         .foregroundColor(.green)
                         .blur(radius: 2)
-                        .shadow(color: .green, radius: greenLightOpacity)    // radius 11
+                        .shadow(color: .green, radius: greenLightShadow)    // radius 11
                     
                         .opacity(greenLightOpacity)
                 }
@@ -83,35 +83,39 @@ struct ContentView: View {
         
         redLightShadow = 11
         greenLightShadow = 0
-        yellowwLightShadow = 0
+        yellowLightShadow = 0
+        
         
     } else if redLightOpacity == 1 && greenLightOpacity == 0.1 && yellowLightOpacity == 0.1 {
+    
+        yellowLightOpacity = 1
+        greenLightOpacity = 0.1
+        redLightOpacity = 0.1
+        
+        yellowLightShadow = 11
+        greenLightShadow = 0
+        redLightShadow = 0
+        
+        
+    } else if redLightOpacity == 0.1 && greenLightOpacity == 0.1 && yellowLightOpacity == 1 {
+       
         greenLightOpacity = 1
         redLightOpacity = 0.1
         yellowLightOpacity = 0.1
         
         redLightShadow = 0
         greenLightShadow = 11
-        yellowwLightShadow = 0
+        yellowLightShadow = 0
+        
         
     } else if redLightOpacity == 0.1 && greenLightOpacity == 1 && yellowLightOpacity == 0.1 {
-        yellowLightOpacity = 1
-        greenLightOpacity = 0.1
-        redLightOpacity = 0.1
-        
-        yellowwLightShadow = 11
-        greenLightShadow = 0
-        redLightShadow = 0
-        
-        
-    } else if redLightOpacity == 0.1 && greenLightOpacity == 0.1 && yellowLightOpacity == 1 {
         redLightOpacity = 1
         greenLightOpacity = 0.1
         yellowLightOpacity = 0.1
         
         redLightShadow = 11
         greenLightShadow = 0
-        yellowwLightShadow = 0
+        yellowLightShadow = 0
         
     } else {
         print ("dsad")
