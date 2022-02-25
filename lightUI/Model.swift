207c51156f78f7e559a13e80a96bdc592b10baf6
Adham <adam.bolotov@gmail.com>
2022-02-25 16:59:32 +0100

    before model

--- lightUI/Model.swift
+++ lightUI/Model.swift
@@ -4,74 +4,74 @@ import SwiftUI
 
 
 
-struct Model {
-    
-    
-     var redLightShadow: Double = 0
-     var yellowLightShadow: Double = 0
-     var greenLightShadow: Double = 0
-    
-     var redLightOpacity: Double = 0.1
-     var greenLightOpacity: Double = 0.1
-     var yellowLightOpacity: Double = 0.1
-    
-    
-    mutating func switchColors() {
-        
-        if redLightOpacity == 0.1 && greenLightOpacity == 0.1 && yellowLightOpacity == 0.1 {
-           
-            redLightOpacity = 1
-            yellowLightOpacity = 0.1
-            greenLightOpacity = 0.1
-            
-            redLightShadow = 11
-            yellowLightShadow = 0
-            greenLightShadow = 0
-            
-            
-            
-        } else if redLightOpacity == 1 && greenLightOpacity == 0.1 && yellowLightOpacity == 0.1 {
-            
-            redLightOpacity = 0.1
-            yellowLightOpacity = 1
-            greenLightOpacity = 0.1
-            
-            
-            redLightShadow = 0
-            yellowLightShadow = 11
-            greenLightShadow = 0
-            
-            
-            
-        } else if redLightOpacity == 0.1 && greenLightOpacity == 0.1 && yellowLightOpacity == 1 {
-            
-            redLightOpacity = 0.1
-            yellowLightOpacity = 0.1
-            greenLightOpacity = 1
-            
-            redLightShadow = 0
-            yellowLightShadow = 0
-            greenLightShadow = 11
-            
-            
-            
-        } else if redLightOpacity == 0.1 && greenLightOpacity == 1 && yellowLightOpacity == 0.1 {
-            
-            redLightOpacity = 1
-            yellowLightOpacity = 0.1
-            greenLightOpacity = 0.1
-            
-            redLightShadow = 11
-            yellowLightShadow = 0
-            greenLightShadow = 0
-            
-        } else {
-            print ("dsad")
-        }
-        
-        
-    }
-}
+//struct Model {
+//
+//
+//     var redLightShadow: Double = 0
+//     var yellowLightShadow: Double = 0
+//     var greenLightShadow: Double = 0
+//
+//     var redLightOpacity: Double = 0.1
+//     var greenLightOpacity: Double = 0.1
+//     var yellowLightOpacity: Double = 0.1
+//
+//
+//    mutating func switchColors() {
+//
+//        if redLightOpacity == 0.1 && greenLightOpacity == 0.1 && yellowLightOpacity == 0.1 {
+//
+//            redLightOpacity = 1
+//            yellowLightOpacity = 0.1
+//            greenLightOpacity = 0.1
+//
+//            redLightShadow = 11
+//            yellowLightShadow = 0
+//            greenLightShadow = 0
+//
+//
+//
+//        } else if redLightOpacity == 1 && greenLightOpacity == 0.1 && yellowLightOpacity == 0.1 {
+//
+//            redLightOpacity = 0.1
+//            yellowLightOpacity = 1
+//            greenLightOpacity = 0.1
+//
+//
+//            redLightShadow = 0
+//            yellowLightShadow = 11
+//            greenLightShadow = 0
+//
+//
+//
+//        } else if redLightOpacity == 0.1 && greenLightOpacity == 0.1 && yellowLightOpacity == 1 {
+//
+//            redLightOpacity = 0.1
+//            yellowLightOpacity = 0.1
+//            greenLightOpacity = 1
+//
+//            redLightShadow = 0
+//            yellowLightShadow = 0
+//            greenLightShadow = 11
+//
+//
+//
+//        } else if redLightOpacity == 0.1 && greenLightOpacity == 1 && yellowLightOpacity == 0.1 {
+//
+//            redLightOpacity = 1
+//            yellowLightOpacity = 0.1
+//            greenLightOpacity = 0.1
+//
+//            redLightShadow = 11
+//            yellowLightShadow = 0
+//            greenLightShadow = 0
+//
+//        } else {
+//            print ("dsad")
+//        }
+//
+//
+//    }
+//}
     
     
     
